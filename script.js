0a275fe33407b43754f2f4b1db49f5a6c21f0a92
souru98 <kulkarnisourabh1998@gmail.com>
2020-02-23 11:34:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,17 @@
 // username should not contain special characters
 // the function should return true if all the conditions above are validated.
 // return false if the validation fails
+function checkUserName() {
+    let username = document.getElementsByClassName('username');
+    if (username.length == 0)
+        return false;
+    if ((username.length >= 8) && (username.length < 15))
+        return false;
+    for (var i = 0; i < username.length; i++)
+        if ((username[i] == " ") || (username[i] == "@") || (username[i] == "&") || (username[i] == "$") || (username[i] == "!"))
+            return false;
+    return true;
+}
 
 // Progression 2 
 // Check mail
@@ -15,6 +26,23 @@
 // only @ , . , _ [at,dot,underscore] is allowed
 // It can be alphanumeric in nature
 // return true if it passses all validation and false otherwise
+function checkEmail() {
+    var mail = document.getElementsByClassName('mail');
+    if (mail == "" || mail == "null") {
+        alert("please enter your email");
+        document.registration.email.focus();
+        return false;
+    }
+    var atpos = mail.indexOf("@");
+    var dotpos = mail.lastIndexOf(".");
+    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= mail.length) {
+        alert("Not a valid e-mail address");
+        return false;
+    }
+    alert("Registration successfull");
+    return true;
+}
+
 
 // Progression 3
 // Check password
@@ -22,3 +50,24 @@
 // should be alphanumeric in nature
 // password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
 // return true else return false
+function checkPassword() {
+    let pass = document.getElementsByClassName('password');
+    var checkSpecial = /[*@!#%&]+/.test(pass);
+    var checkUpper = /[A-Z]+/.test(pass);
+    var checkLower = /[a-z]+/.test(pass);
+    if (pass == "" || pass == "null") {
+        alert("please enter ur password");
+        document.registration.pwd.focus();
+        return false;
+    }
+    if (checkSpecial && checkLower && checkUpper) {
+        flag = 1;
+    }
+    if (flag == 0) {
+        alert("Password should be the combination of atleast one lowercase letter, uppercaseletter & special symbols like($, & , #)");
+        document.registration.pwd.focus();
+        return false;
+    }
+    alert("Registration successfull");
+    return true;
+}
