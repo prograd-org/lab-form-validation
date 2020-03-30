6c6df97aab92c42f20c808c52f92bafe1cef7788
PRiNCE BHARDWAj <Prince>
2020-03-31 02:10:15 +0530

    FSJ0320A021

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 // Progression 1
-// Check username
-// create a function called as checkUserName()
-// username should not be empty
-// username should be a minimum of 8 characters and above
-// username should be less than 15 characters
-// No space is allowed in username
-// username should not contain special characters
-// the function should return true if all the conditions above are validated.
-// return false if the validation fails
+
 var button = document.getElementById("submit");
 button.addEventListener("click", checkUserName);
 
@@ -32,11 +24,7 @@ function checkUserName() {
 }
 
 // Progression 2
-// Check mail
-// create a function called as checkEmail()
-// only @ , . , _ [at,dot,underscore] is allowed
-// It can be alphanumeric in nature
-// return true if it passses all validation and false otherwise
+
 button.addEventListener("click", checkEmail);
 
 function checkEmail() {
@@ -46,11 +34,7 @@ function checkEmail() {
 }
 
 // Progression 3
-// Check password
-// create a function called as checkPassword()
-// should be alphanumeric in nature
-// password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
-// return true else return false
+
 button.addEventListener("click", checkPassword);
 
 function checkPassword() {
