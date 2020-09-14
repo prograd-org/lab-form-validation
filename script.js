172b0db64eadfe0b3bdd4c2fe5ce248f2d3d5000
sawankrgope <sawankrgope@gmail.com>
2020-09-14 09:13:30 +0530

    16114151

--- script.js
+++ script.js
@@ -8,6 +8,29 @@
 // username should not contain special characters
 // the function should return true if all the conditions above are validated.
 // return false if the validation fails
+let button = document.getElementById("submit");
+button.addEventListener("click", checkUserName);
+button.addEventListener("click", checkEmail);
+button.addEventListener("click", checkPassword);
+
+
+function checkUserName() {
+    let username = document.getElementById('usertext').value;
+    var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/; //special characters 
+    if (format.test(username)) {
+        alert("You have entered an invalid USERNAME!")
+        return (false)
+    } else if (username != "" && username.length > 7 && username.length < 15 && username.indexOf(' ') < 0) {
+        {
+            return (true)
+        }
+
+    } else {
+        alert("You have entered an invalid USERNAME!")
+        return (false)
+
+    }
+}
 
 // Progression 2 
 // Check mail
@@ -16,9 +39,29 @@
 // It can be alphanumeric in nature
 // return true if it passses all validation and false otherwise
 
+function checkEmail() {
+    let email = document.getElementById("emailtext").value;
+
+    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
+        return (true)
+    }
+
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+
 // Progression 3
 // Check password
 // create a function called as checkPassword()
 // should be alphanumeric in nature
 // password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
 // return true else return false
+
+function ValidatePswd(password) {
+    var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    if (password.match(passw))
+        alert("Password is validated");
+    else
+        alert("password must contain atleast one Uppercase, one number and special characters like[!@#$%^&*()_]")
+} 
+
